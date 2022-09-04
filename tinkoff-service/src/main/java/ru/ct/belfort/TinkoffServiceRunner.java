c66908e957a49439c16c6320246c9ef997fc8e2d
SamurajKing <40471961+SamurajKing@users.noreply.github.com>
2022-09-04 23:51:39 +0300

    added tinkoff-service (#4)
    * added tinkoff-service
    
    * fixes

---
+++ tinkoff-service/src/main/java/ru/ct/belfort/TinkoffServiceRunner.java
@@ -0,0 +1,11 @@
+package ru.ct.belfort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffServiceRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffServiceRunner.class, args);
+    }
+}
