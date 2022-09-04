df3b4595e2cfa8d289daf78fd3eed729aef0d113
German Andosov <334875@niuitmo.ru>
2022-09-04 11:18:32 +0300

    module adjustments

--- trading-bot/src/main/java/ru/ct/belfort/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.ct.belfort;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Trading bot service is working!");
-    }
-}
---
+++ trading-bot/src/main/java/ru/ct/belfort/RestApiController.java
@@ -0,0 +1,15 @@
+package ru.ct.belfort;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trading")
+public class RestApiController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Trading bot is working!";
+    }
+}
