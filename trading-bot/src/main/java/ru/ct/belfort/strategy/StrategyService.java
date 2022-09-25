abe665c7b94193398e06f448387eb6eb957b01f7
German Andosov <334875@niuitmo.ru>
2022-09-25 16:33:26 +0300

    list<strats> -> map<strats>

--- trading-bot/src/main/java/ru/ct/belfort/strategy/StrategyService.java
+++ trading-bot/src/main/java/ru/ct/belfort/strategy/StrategyService.java
@@ -1,32 +1,39 @@
 package ru.ct.belfort.strategy;
 
 import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.ct.belfort.IdeaDTO;
 import ru.ct.belfort.TradingInfoDTO;
 import ru.ct.belfort.kafka.producers.IdeasProducer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class StrategyService {
 
     IdeasProducer ideasProducer;
-    List<StrategyInterface> strats;
+    Map<String, StrategyInterface> strats;
+
+    @Autowired
+    public StrategyService(IdeasProducer ideasProducer, List<StrategyInterface> stratsList) {
+        this.ideasProducer = ideasProducer;
+        this.strats = new HashMap<>();
+        for (StrategyInterface strat : stratsList) {
+            strats.put(strat.getQualifier(), strat);
+        }
+    }
 
     public void dispense(TradingInfoDTO dto) {
-        double result = -1;
-        System.out.println(strats.size());
-        for (StrategyInterface strat : strats) {
-            if (dto.strategy().equals(strat.getQualifier())) {
-                result = strat.predict(dto.candles());
-                break;
-            }
+        if (!strats.containsKey(dto.strategy())) {
+            throw new RuntimeException("Unknown strategy");
+            // TODO: provide error to tinkoff-service?
         }
+        double result = strats.get(dto.strategy()).predict(dto.candles());
 
         IdeaDTO idea = new IdeaDTO(result, "Some meta info");
         ideasProducer.sendMessage(idea);
