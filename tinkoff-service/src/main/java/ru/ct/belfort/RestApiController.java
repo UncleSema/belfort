b5d3a452e6072d278c4d63e1a8fe11b42f4f6b29
durmanko <kott9radevelop@gmail.com>
2022-10-15 00:18:39 +0300

    env name fix

--- tinkoff-service/src/main/java/ru/ct/belfort/RestApiController.java
+++ tinkoff-service/src/main/java/ru/ct/belfort/RestApiController.java
@@ -16,16 +16,16 @@ public class RestApiController {
     @NonNull
     private final TestProducer producer;
 
-    private final String TINKOFF_TOKEN = System.getenv("tinkoff_token");
+    private final String tinkoffToken = System.getenv("TINKOFF_TOKEN");
 
     @GetMapping("/tinkoff-service")
     public String hello() {
-        return "Tinkoff service is working! Token=" + TINKOFF_TOKEN;
+        return "Tinkoff service is working! Token=" + tinkoffToken;
     }
     @GetMapping("/tinkoff-api-test")
     public String tinkoff() {
         producer.sendMessage(new UserDTO(1,
-                TINKOFF_TOKEN,
+                tinkoffToken,
                 "someStrategy",
                 List.of("BBG004730ZJ9")));
         return "Tinkoff api is working!";
