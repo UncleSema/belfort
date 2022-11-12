bc72adb766ed46acac6b85b9f77ee82e23e4462d
Eric <ericdygin@mail.ru>
2022-11-12 11:14:18 +0300

    added telegram bot

--- telegram-service/src/main/java/ru/ct/belfort/RestApiController.java
+++ telegram-service/src/main/java/ru/ct/belfort/RestApiController.java
@@ -3,7 +3,11 @@ package ru.ct.belfort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.ct.belfort.kafka.producers.TestProducer;
+import ru.ct.belfort.tgbot.TelegramBot;
 
 import java.util.List;
 
@@ -14,8 +18,14 @@ public class RestApiController {
 
     private final TestProducer producer;
 
-    public RestApiController(TestProducer producer) {
+    public RestApiController(TestProducer producer, TelegramBot bot) {
         this.producer = producer;
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -31,6 +41,7 @@ public class RestApiController {
                 "token",
                 "strat",
                 List.of("1")));
+
         return "Telegram service is working!";
     }
 
