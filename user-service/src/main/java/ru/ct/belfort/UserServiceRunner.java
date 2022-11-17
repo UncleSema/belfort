868bceddbb71d573a5835767e361a244e764c71b
Alexey Grunskii <you@example.com>
2022-11-17 22:46:26 +0300

    Initial commit which shows working address https://localhost:8080/greeting?name=Semen

---
+++ user-service/src/main/java/ru/ct/belfort/UserServiceRunner.java
@@ -0,0 +1,11 @@
+package ru.ct.belfort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceRunner.class, args);
+    }
+}
