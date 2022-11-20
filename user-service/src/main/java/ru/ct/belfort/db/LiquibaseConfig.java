53eb885dda60eeec7a7577ffc6a0b3133c513312
Alexey Grunskii <you@example.com>
2022-11-20 18:00:47 +0300

    Move on field injection with constructor && Add Objects.requireNonNull

--- user-service/src/main/java/ru/ct/belfort/db/LiquibaseConfig.java
+++ user-service/src/main/java/ru/ct/belfort/db/LiquibaseConfig.java
@@ -1,7 +1,6 @@
 package ru.ct.belfort.db;
 
 import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,19 +8,22 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @PropertySource("classpath:database.properties")
 public class LiquibaseConfig {
+    final Environment env;
 
-    @Autowired
-    Environment env;
+    LiquibaseConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
-    public DataSource dataSource()  {
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.drivername"));
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.drivername")));
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
